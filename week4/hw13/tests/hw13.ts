20492a4cf1c62748c192ffafcdcd31911d5b103a
chethack <techbuddy022@gmail.com>
2023-10-02 23:28:39 +0530

    test fixed thanks to @patidar007

--- week4/hw13/tests/hw13.ts
+++ week4/hw13/tests/hw13.ts
@@ -1,25 +1,24 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Hw13 } from "../target/types/hw13";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 describe("hw13", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
 
   const program = anchor.workspace.Hw13 as Program<Hw13>;
 
   it("Initializes account with balance 100", async () => {
     const myAccount = anchor.web3.Keypair.generate();
-    const user = anchor.web3.Keypair.generate();
+
 
     const sig = await program.methods.initialize()
   .accounts({
       myAccount: myAccount.publicKey,
-      user: user.publicKey, 
+      user: provider.publicKey, 
       systemProgram: anchor.web3.SystemProgram.programId,
   })
-  .signers([myAccount, user])
+  .signers([myAccount])
   .rpc();
     const account = await program.account.myAccount.fetch(myAccount.publicKey);
     assert.equal(account.balance, 100);
