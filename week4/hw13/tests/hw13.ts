9670446748f86f8dde655f96a845bc2bdfd69a86
chethack <techbuddy022@gmail.com>
2023-10-09 21:35:40 +0530

    test now working, added one more test, thanks to @patidar007 on discord

--- week4/hw13/tests/hw13.ts
+++ week4/hw13/tests/hw13.ts
@@ -6,11 +6,9 @@ import { assert, expect } from "chai";
 describe("hw13", () => {
   const provider = anchor.AnchorProvider.env();
   const program = anchor.workspace.Hw13 as Program<Hw13>;
-  // Create and initialize myAccount once, outside of the individual tests
   const myAccount = anchor.web3.Keypair.generate();
   
   before(async () => {
-    // Initialize myAccount before running the tests
     await program.methods.initialize()
       .accounts({
         myAccount: myAccount.publicKey,
@@ -33,10 +31,24 @@ describe("hw13", () => {
     user: provider.publicKey,
     systemProgram: anchor.web3.SystemProgram.programId,
   })
-  .signers([myAccount, provider.wallet.payer]) 
   .rpc();
 
     const account = await program.account.myAccount.fetch(myAccount.publicKey);
     assert.equal(account.balance, 200);
   });
+
+  it("Does not increment balance over 1000", async () => { 
+  try {
+    for (let i = 0; i < 10; i++) {
+      await program.methods.increment100()
+        .accounts({
+          myAccount: myAccount.publicKey,
+          user: provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        });
+    }
+  } catch (err) {
+    assert.ok(err.toString().includes("Cannot increment by 100"));
+  }
+});
 });
